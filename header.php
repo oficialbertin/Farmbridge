755cf9db85361544310382cbebac6c20cd41ee97
oficialbertin <oficialbertin@gmail.com>
2025-10-20 22:00:57 +0200

    we

--- header.php
+++ header.php
@@ -42,6 +42,7 @@ if (isset($_SESSION['user_id'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css">
     
 <style>
         :root {
